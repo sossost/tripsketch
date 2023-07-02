c709ab3d56cd300134fcea4f945a77886695c96a
jang_yoonsu <shagrat8546@gmail.com>
2023-07-02 23:35:58 +0900

    Feat : 프로필 컴포넌트 메모이제이션

--- src/components/user/Profile.tsx
+++ src/components/user/Profile.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { StyleSheet, Image, Text, View, TouchableOpacity } from "react-native";
 
 type ProfileProps = {
@@ -42,7 +43,8 @@ const Profile = (props: ProfileProps) => {
   );
 };
 
-export default Profile;
+// 부모컴포넌트의 리렌더링은 대부분 카테고리가 변경될때 이므로 프로필 컴포넌트 메모이제이션
+export default React.memo(Profile);
 
 const styles = StyleSheet.create({
   container: {
