508d0d22e6c03fdcc15b84609fe18da4df05b8d5
jang_yoonsu <shagrat8546@gmail.com>
2023-07-03 14:16:04 +0900

    Chore : 리액트 쿼리 설치 및 세팅

--- App.tsx
+++ App.tsx
@@ -2,8 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import RootStack from "./src/navigation/RootStack";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 export default function App() {
-  return <RootStack />;
+  <QueryClientProvider client={queryClient}>
+    return <RootStack />;
+  </QueryClientProvider>;
 }
 
 const styles = StyleSheet.create({
