93bbb74b278549eb8f3ae2bbd0ea30163c6b6c77
limeorange <happyshsh72@gmail.com>
2023-07-04 04:07:02 +0900

    Refactor : App.tsx 사용하지 않는 코드 정리

--- App.tsx
+++ App.tsx
@@ -1,16 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import RootStack from "./src/navigation/RootStack";
 
 export default function App() {
   return <RootStack />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
