e3fef2066e7acbbf8e91ec27adf474e01b6279e6
ksy <ksoup3434@gmail.com>
2023-07-04 10:21:22 +0900

    fix: app 오류 수정

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import RootStack from "./src/navigation/RootStack";
+import { StatusBar } from "expo-status-bar";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 export default function App() {
-  <QueryClientProvider client={queryClient}>
-    return <RootStack />;
-  </QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <StatusBar style="auto" />
+      <RootStack />
+    </QueryClientProvider>
+  );
 }
