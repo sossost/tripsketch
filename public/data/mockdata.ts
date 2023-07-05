9892f01c4fb8f2627cab315110d8f8f3d3ec7542
jang_yoonsu <shagrat8546@gmail.com>
2023-07-05 16:03:02 +0900

    Feat : 목데이터 파일 추가

---
+++ public/data/mockdata.ts
@@ -0,0 +1,85 @@
+export const currentUser = {
+  id: "elice_official",
+  profile_img: "https://eliceproject.s3.ap-northeast-2.amazonaws.com/dongs.png",
+  followerList: ["나", "너", "우리"],
+  followingList: ["너", "그", "그녀"],
+  user_name: "엘리스",
+  introduction: "프로필 불라불라 어쩌구저쩌구",
+};
+
+export const category = ["전체보기", "프랑스", "독일", "이탈리아"];
+
+export const users = [
+  {
+    id: "user1",
+    profile_img:
+      "https://eliceproject.s3.ap-northeast-2.amazonaws.com/dongs.png",
+    followerList: ["user2", "user3", "user4"],
+    followingList: ["user2", "user3"],
+    user_name: "유저네임1",
+    introduction: "유저네임1의 프로필 입니다.",
+  },
+  {
+    id: "user2",
+    profile_img:
+      "https://eliceproject.s3.ap-northeast-2.amazonaws.com/dongs.png",
+    followerList: ["user1", "user3", "user4"],
+    followingList: ["user1", "user3"],
+    user_name: "유저네임2",
+    introduction: "유저네임2의 프로필 입니다.",
+  },
+  {
+    id: "user3",
+    profile_img:
+      "https://eliceproject.s3.ap-northeast-2.amazonaws.com/dongs.png",
+    followerList: ["user2", "user1", "user4"],
+    followingList: ["user2", "user4"],
+    user_name: "유저네임3",
+    introduction: "유저네임3의 프로필 입니다.",
+  },
+  {
+    id: "user4",
+    profile_img:
+      "https://eliceproject.s3.ap-northeast-2.amazonaws.com/dongs.png",
+    followerList: ["user2", "user3", "user1"],
+    followingList: ["user2", "user3"],
+    user_name: "유저네임4",
+    introduction: "유저네임4의 프로필 입니다.",
+  },
+  {
+    id: "user1",
+    profile_img:
+      "https://eliceproject.s3.ap-northeast-2.amazonaws.com/dongs.png",
+    followerList: ["user2", "user3", "user4"],
+    followingList: ["user2", "user3"],
+    user_name: "유저네임1",
+    introduction: "유저네임1의 프로필 입니다.",
+  },
+  {
+    id: "user2",
+    profile_img:
+      "https://eliceproject.s3.ap-northeast-2.amazonaws.com/dongs.png",
+    followerList: ["user1", "user3", "user4"],
+    followingList: ["user1", "user3"],
+    user_name: "유저네임2",
+    introduction: "유저네임2의 프로필 입니다.",
+  },
+  {
+    id: "user3",
+    profile_img:
+      "https://eliceproject.s3.ap-northeast-2.amazonaws.com/dongs.png",
+    followerList: ["user2", "user1", "user4"],
+    followingList: ["user2", "user4"],
+    user_name: "유저네임3",
+    introduction: "유저네임3의 프로필 입니다.",
+  },
+  {
+    id: "user4",
+    profile_img:
+      "https://eliceproject.s3.ap-northeast-2.amazonaws.com/dongs.png",
+    followerList: ["user2", "user3", "user1"],
+    followingList: ["user2", "user3"],
+    user_name: "유저네임4",
+    introduction: "유저네임4의 프로필 입니다.",
+  },
+];
