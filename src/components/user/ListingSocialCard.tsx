b554224b5b1955fafed851022b3dd857bb3d8f44
jang_yoonsu <shagrat8546@gmail.com>
2023-07-05 16:00:10 +0900

    Feat : 소셜카드리스트 컴포넌트 구현

---
+++ src/components/user/ListingSocialCard.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, FlatList, View } from "react-native";
+import SocialCard from "./SocialCard";
+
+type ListingSocialCard = {
+  currentUser: User | undefined;
+  userList: User[];
+};
+
+const ListingSocialCard = (props: ListingSocialCard) => {
+  const { currentUser, userList } = props;
+  return (
+    <FlatList
+      data={userList}
+      numColumns={2}
+      contentContainerStyle={styles.cardContainer}
+      renderItem={(userData) => {
+        const user = userData.item;
+        const isFollowing = currentUser
+          ? currentUser?.followingList.includes(user.id)
+          : false;
+        return <SocialCard user={user} isFollowing={isFollowing} />;
+      }}
+      alwaysBounceVertical={false}
+    />
+  );
+};
+
+export default ListingSocialCard;
