54c962b3bf36dff68570b6ad264fae821ed443bc
yoonsu <110542210+sossost@users.noreply.github.com>
2023-07-13 14:45:40 +0900

    Revert "Feature/edit profile page"

--- src/screens/MyPage.tsx
+++ src/screens/MyPage.tsx
@@ -9,7 +9,6 @@ import Category from "../components/user/Category";
 import DiaryList from "../components/user/DiaryList";
 
 import { category, currentUser } from "../../data/mockdata";
-import { useNavigation } from "@react-navigation/native";
 
 const MyPage = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>("전체보기");
@@ -21,14 +20,14 @@ const MyPage = () => {
     getDiariesByCategory(selectedCategory)
   );
 
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
       <Profile
         variant="myPage"
         user={currentUser}
-        onPress={() => navigation.navigate("EditProfilePage" as never)}
+        onPress={() => console.log("ㅎㅇ")}
       />
       <Category
         category={category}
