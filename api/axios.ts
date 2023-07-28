9f72434d497b2ed271dd645d70fbbadbad66b268
limeorange <happyshsh72@gmail.com>
2023-07-28 15:15:16 +0900

    Feat : axiosBase 인스턴스 생성

--- api/axios.ts
+++ api/axios.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-// Axios 인스턴스 생성
+/** axiosBase 인스턴스 생성 */
 export const axiosBase = axios.create({
   baseURL: "https://port-0-tripsketch-kvmh2mljz6ccl7.sel4.cloudtype.app",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
 });
