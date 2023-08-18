8637ebaf109bf5d7da63f2eb978ad4d4bfbf29a4
jang_yoonsu <shagrat8546@gmail.com>
2023-08-18 16:59:34 +0900

    Refactor : 유저, 마이페이지 분리

--- src/navigation/BottomTab.tsx
+++ src/navigation/BottomTab.tsx
@@ -7,6 +7,7 @@ import MyPage from "../screens/MyPage";
 import TripList from "../screens/TripList";
 import Notice from "../screens/Notice";
 import CreatePost from "../screens/CreatePost";
+import UserPage from "../screens/UserPage";
 
 const Tabs = createBottomTabNavigator();
 
