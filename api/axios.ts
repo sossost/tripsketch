081019848518439006eb5fd29f92e311aa159f30
limeorange <happyshsh72@gmail.com>
2023-08-21 18:22:00 +0900

    Feat : 액세스 토큰 재발급 인터셉터 설정

--- api/axios.ts
+++ api/axios.ts
@@ -1,30 +1,7 @@
+import { useNavigation } from "@react-navigation/native";
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 import jwtDecode, { JwtPayload } from "jwt-decode";
-
-/** 액세스 토큰 가져오는 함수 */
-const getAccessToken = async () => {
-  return await SecureStore.getItemAsync("accessToken");
-};
-
-/** 액세스 토큰 만료 여부 판단하는 함수 */
-const isTokenExpired = async (accessToken: string) => {
-  try {
-    const decodedToken: JwtPayload = jwtDecode(accessToken);
-    if (!decodedToken || typeof decodedToken.exp !== "number") {
-      // 토큰이 유효하지 않거나 만료 시간 정보가 없는 경우
-      return true; // 토큰 만료로 간주
-    }
-
-    const currentTimestampInSeconds = Math.floor(Date.now() / 1000); // 현재 시간을 초 단위로 변환
-    return currentTimestampInSeconds >= decodedToken.exp; // 현재 시간과 토큰의 만료 시간 비교
-  } catch (error) {
-    return true; // 토큰 디코드 또는 처리 과정에서 에러가 발생한 경우 토큰 만료로 간주
-  }
-};
-
-/** 액세스 토큰 만료 시 리프레시 토큰으로 갱신하는 함수 */
-// const tokenRefresh =
 
 /** axiosBase 인스턴스 생성 */
 export const axiosBase = axios.create({
@@ -32,22 +9,90 @@ export const axiosBase = axios.create({
   timeout: 10000, // 요청 대기 시간 10초로 설정
 });
 
-// 요청 인터셉터: 모든 요청 전에 실행되는 함수
-axiosBase.interceptors.request.use(
-  (config) => {
-    const accessToken = getAccessToken();
+/** 액세스 토큰 가져오는 함수 */
+const getAccessToken = async () => {
+  return await SecureStore.getItemAsync("accessToken");
+};
 
-    // 요청 헤더에 토큰과 컨텐트 타입을 설정합니다.
-    config.headers["Content-Type"] = "application/json";
-    config.headers["Authorization"] = `Bearer ${accessToken}`;
+/** 액세스 토큰 만료 여부 판단하는 함수 */
+const isTokenExpired = async () => {
+  const accessToken = await getAccessToken();
+  if (accessToken !== null) {
+    try {
+      const decodedToken: JwtPayload = jwtDecode(accessToken);
 
-    return config; // 수정된 설정(config)을 반환하여 요청을 계속 진행합니다.
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error); // 에러가 발생하면 해당 에러를 반환합니다.
+      // 토큰이 유효하지 않거나 만료 시간 정보가 없는 경우 토큰 만료로 간주
+      if (!decodedToken || typeof decodedToken.exp !== "number") {
+        return true;
+      }
+      // 현재 시간을 초 단위로 변환
+      const currentTimestampInSeconds = Math.floor(Date.now() / 1000);
+
+      if (currentTimestampInSeconds >= decodedToken.exp) {
+        console.log("액세스 토큰이 만료되었습니다!");
+      } else {
+        console.log("액세스 토큰이 유효합니다");
+      }
+
+      // 현재 시간과 토큰의 만료 시간 비교
+      return currentTimestampInSeconds >= decodedToken.exp;
+    } catch (error) {
+      // 토큰 디코드 또는 처리 과정에서 에러가 발생한 경우 토큰 만료로 간주
+      return true;
+    }
+  } else {
+    // accessToken이 null인 경우 토큰 만료로 간주
+    return true;
   }
-);
+};
+
+/** 리프레시 토큰으로 액세스 토큰 갱신하는 함수 */
+export const tokenRefresh = async () => {
+  const navigation = useNavigation();
+
+  // 리프레시 토큰으로 액세스 토큰 갱신 요청
+  try {
+    const refreshToken = await SecureStore.getItemAsync("refreshToken");
+
+    if (refreshToken) {
+      const response = await axiosBase.post("/api/oauth/kakao/refreshToken", {
+        ourRefreshToken: refreshToken,
+      });
+
+      // 새로운 액세스 토큰 저장
+      const newAccessToken = response.data.accessToken;
+      console.log(
+        "리프레시 토큰으로 새로 발근한 액세스 토큰은...",
+        newAccessToken
+      );
+      await SecureStore.setItemAsync("accessToken", newAccessToken);
+    } else {
+      console.log("리프레시 토큰이 없습니다!!");
+      // 리프레시 토큰이 만료된 경우 로그인 화면으로 이동
+      navigation.navigate as (route: string) => void;
+      ("Login");
+    }
+  } catch (error) {
+    console.log("리프레시 토큰 갱신 요청 중 발생한 에러는...🤔", error);
+  }
+};
+
+// 요청 인터셉터: 모든 요청 전에 실행되는 함수
+// axiosBase.interceptors.request.use(
+//   (config) => {
+//     const accessToken = getAccessToken();
+
+//     // 요청 헤더에 토큰과 컨텐트 타입을 설정합니다.
+//     config.headers["Content-Type"] = "application/json";
+//     config.headers["Authorization"] = `Bearer ${accessToken}`;
+
+//     return config; // 수정된 설정(config)을 반환하여 요청을 계속 진행합니다.
+//   },
+//   (error) => {
+//     console.log(error);
+//     return Promise.reject(error); // 에러가 발생하면 해당 에러를 반환합니다.
+//   }
+// );
 
 // 응답 인터셉터: 모든 응답 전에 실행되는 함수
 axiosBase.interceptors.response.use(
@@ -61,7 +106,7 @@ axiosBase.interceptors.response.use(
   async (error) => {
     if (error.response?.status === 401) {
       // 응답 상태 코드가 401 (Unauthorized)인 경우
-      if (isTokenExpired()) await tokenRefresh(); // 토큰이 만료되었다면 토큰을 갱신합니다.
+      if (await isTokenExpired()) await tokenRefresh(); // 토큰이 만료되었다면 토큰을 갱신합니다.
 
       const accessToken = getAccessToken(); // 갱신된 토큰을 가져옵니다.
 
