b2aa5f034015196455b26aef9b43c9f8d1edaffc
jang_yoonsu <shagrat8546@gmail.com>
2023-08-23 17:06:59 +0900

    Refactor : api BaseUrl 변경 및 env로 분리

---
+++ env.d.ts
@@ -0,0 +1,6 @@
+declare module "@env" {
+  export const S3_ACCESS_KEY_ID: string;
+  export const S3_SECRET_ACCESS_KEY: string;
+  export const S3_BUCKET_NAME: string;
+  export const API_BASE_URL: string;
+}
