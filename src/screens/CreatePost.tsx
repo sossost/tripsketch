627cb205757d097c3ad8df58f1f033f07eee8534
ksy <ksoup3434@gmail.com>
2023-09-07 19:37:32 +0900

    Feat: 이미지 업로드 ui 변경및 api 연결

--- src/screens/CreatePost.tsx
+++ src/screens/CreatePost.tsx
@@ -7,16 +7,23 @@ import {
   Image,
   Button,
   TouchableWithoutFeedback,
+  Dimensions,
 } from "react-native";
 import { Calendar, LocaleConfig } from "react-native-calendars";
 import styled from "styled-components/native";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
+import { colors } from "../constants/color";
 import React, { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 
 import MapView, { Marker, UrlTile } from "react-native-maps";
+import Toast from "react-native-toast-message";
+
+// 테스트 추가 - 정리 필요
+import axios from "axios";
+import { useCreatePost } from "../hooks/usePostQuery";
 
 type Suggestion = {
   place_id: string;
@@ -95,7 +102,12 @@ const CreatePost: React.FC = () => {
   const [query, setQuery] = useState<string>("");
   const [suggestions, setSuggestions] = useState<Suggestion[]>([]);
   const [mapViewOn, setMapViewOn] = useState(false);
+  const [locationName, setLocationName] = useState("");
   const [address, setAddress] = useState({
+    countryCode: "",
+    address: "",
+    municipality: "",
+    name: "",
     country: "",
     city: "",
     town: "",
@@ -111,12 +123,14 @@ const CreatePost: React.FC = () => {
     longitudeDelta: 38.48174795508386,
   });
 
+  //console.log(address);
+
   const [showModal, setShowModal] = useState(false);
   const [markedDates, setMarkedDates] = useState<RangeKeyDict>({});
   const [selectedDates, setSelectedDates] = useState<string[]>([]);
   const [startDate, setStartDate] = useState<string | null>(null);
   const [endDate, setEndDate] = useState<string | null>(null);
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState<string[]>([]);
 
   // 230728
 
@@ -153,6 +167,10 @@ const CreatePost: React.FC = () => {
       .then((response) => response.json())
       .then((data) =>
         setAddress({
+          countryCode: data.address.country_code,
+          address: data.address.country,
+          municipality: data.address.province,
+          name: data.name,
           country: data.address.country,
           city: data.address.city,
           town: data.address.town,
@@ -199,6 +217,10 @@ const CreatePost: React.FC = () => {
         .then((response) => response.json())
         .then((data) =>
           setAddress({
+            countryCode: data.address.country_code,
+            address: data.address.country,
+            municipality: data.address.province,
+            name: data.name,
             country: data.address.country,
             city: data.address.city,
             town: data.address.town,
@@ -219,19 +241,78 @@ const CreatePost: React.FC = () => {
     }, 1000); // 1초 후에 실행
   };
 
+  const selectLocationHandler = (query: string) => {
+    setLocationName(query);
+    setMapViewOn(false);
+  };
+
+  /** 이미지 수 최대 10개로 제한 */
+  const maxImageCount = 10;
+
+  /** 화면 너비 가져오기  */
+  const windowWidth = Dimensions.get("window").width - 40;
+  const imageWidth = windowWidth * 0.19;
+
   const pickImage = async () => {
+    // 이미지 초과 시 알림
+    if (image.length >= maxImageCount) {
+      alert(`이미 ${maxImageCount}장을 선택하셨습니다.`);
+      return;
+    }
+
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
+      allowsEditing: false,
       aspect: [4, 3],
-      quality: 1,
-      //allowsMultipleSelection: true,
+      quality: 0.4,
+      allowsMultipleSelection: true,
+      selectionLimit: maxImageCount,
     });
 
-    console.log(result);
-
     if (!result.canceled) {
-      setImage(result.uri);
+      const selectedUris = result.assets.map((asset) => asset.uri);
+      const newImages = [...image, ...selectedUris];
+
+      if (newImages.length > maxImageCount) {
+        alert(
+          `이미지 선택 제한을 초과했습니다. 최대 ${maxImageCount}장을 선택할 수 있습니다.`
+        );
+      } else {
+        setImage(newImages);
+      }
+    }
+  };
+
+  const submitImage = async (image: string[]) => {
+    console.log(image);
+    try {
+      const formData: any = new FormData();
+
+      for (let index = 0; index < image.length; index++) {
+        const item = image[index];
+        formData.append("files", {
+          uri: item,
+          name: `image${index}.jpg`,
+          type: "multipart/form-data",
+          //type: "image/jpeg",
+        });
+      }
+
+      console.log(formData);
+      const response = await axios.post(
+        "https://port-0-tripsketch-kvmh2mljz6ccl7.sel4.cloudtype.app/api/user/uploads?dir=tripsketch",
+        formData,
+        {
+          headers: {
+            "content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log("Upload response:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Image upload error:", error);
     }
   };
 
@@ -281,6 +362,71 @@ const CreatePost: React.FC = () => {
       currentDate.setDate(currentDate.getDate() + 1);
     }
     return rangeDates;
+  };
+
+  /* 게시물 등록 함수 */
+  const createPostMutation = useCreatePost();
+
+  const submitPost = async () => {
+    try {
+      // const postData = {
+      //   title: "안녕하세요!!",
+      //   content: "담백한~ 크래커 헣흐허허",
+      //   location: locationName,
+      //   startedAt: startDate,
+      //   endAt: endDate,
+      //   latitude: address.latitude,
+      //   longitude: address.longitude,
+      //   hashtagInfo: {
+      //     countryCode: "string",
+      //     country: "string",
+      //     city: "string",
+      //     municipality: "string",
+      //     name: "string",
+      //     displayName: "string",
+      //     road: "string",
+      //     address: "string",
+      //     etc: ["string"],
+      //   },
+      //   isPublic: true,
+      //   images: [
+      //     "https://images.unsplash.com/photo-1691938670117-5c65ae4c19fe?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5Mzk5MTEwMg&ixlib=rb-4.0.3&q=80&w=1080",
+      //   ],
+      // };
+
+      const postData = {
+        id: "",
+        title: "당근서연",
+        content: "안녕",
+        location: "대한민국",
+        startedAt: "2023-08-30T07:52:04.492Z",
+        endAt: "2023-08-30T07:52:04.492Z",
+        latitude: 37.5642135,
+        longitude: 127.0016985,
+        hashtagInfo: {
+          countryCode: "kr",
+          country: "대한민국",
+          city: "서울",
+          municipality: "",
+          name: "",
+          displayName:
+            "퇴계로51길, 오장동, 광희동, 중구, 서울, 04618, 대한민국",
+          road: "퇴계로51길",
+          address: "",
+          etc: ["한국도착", "국내여행", "퇴사기념"],
+        },
+        isPublic: true,
+        updatedAt: "2023-08-30T07:52:04.492Z",
+        images: [
+          "https://images.unsplash.com/photo-1691938670117-5c65ae4c19fe?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5Mzk5MTEwMg&ixlib=rb-4.0.3&q=80&w=1080",
+        ],
+      };
+      await createPostMutation.mutateAsync(postData);
+      Toast.show({ type: "success", text1: "댓글 생성이 완료되었습니다." });
+    } catch (error) {
+      console.error("게시물 생성 중 오류 발생:", error);
+      Toast.show({ type: "error", text1: "게시물 생성을 실패하였습니다." });
+    }
   };
 
   return (
@@ -399,7 +545,7 @@ const CreatePost: React.FC = () => {
             <MapIcon name="map-marker-alt" />
             <Title>여행지</Title>
             <ContentText onPress={() => setMapViewOn(!mapViewOn)}>
-              선택
+              {query === "" ? "선택" : locationName}
             </ContentText>
           </InfoBox>
         </HeaderInfo>
@@ -418,7 +564,12 @@ const CreatePost: React.FC = () => {
                 onChangeText={handleInputChange}
                 placeholder="도시 이름을 입력해주세요."
               />
-              <FontAwesome name="check" size={24} color="#73bbfb" />
+              <FontAwesome
+                name="check"
+                size={24}
+                color="#73bbfb"
+                onPress={() => selectLocationHandler(query)}
+              />
               {query.length === 0 ? (
                 <></>
               ) : suggestions.length > 0 ? (
@@ -493,14 +644,7 @@ const CreatePost: React.FC = () => {
             {/* 내용 */}
             <ContentPhotoBox>
               <Title>내용</Title>
-
-              {/* <PhotoIcon name="photo" onPress={pickImage} />
-              {image && (
-                <Image
-                  source={{ uri: image }}
-                  style={{ width: 200, height: 200 }}
-                />
-              )} */}
+              <PhotoIcon name="photo" onPress={pickImage} />
             </ContentPhotoBox>
             {/* <ScrollView
             contentContainerStyle={{ flexGrow: 1 }}
@@ -517,14 +661,23 @@ const CreatePost: React.FC = () => {
             {/* 이미지 업로드 */}
             <Title>이미지</Title>
             <PickImageButton onPress={pickImage}>
-              <Text>추가 버튼</Text>
+              <PickImageButtonText>추가</PickImageButtonText>
             </PickImageButton>
-            {image && (
-              <Image
-                source={{ uri: image }}
-                style={{ width: 30, height: 30 }}
-              />
-            )}
+            <ImageViewContainer imageCount={image.length}>
+              {image.map((imageUri, index) => (
+                <Image
+                  key={index}
+                  source={{ uri: imageUri }}
+                  style={{ width: imageWidth, height: imageWidth }}
+                />
+              ))}
+            </ImageViewContainer>
+            <TouchableOpacity
+              style={{ width: 50, height: 20, backgroundColor: "#a85757" }}
+              onPress={() => submitImage(image)}
+            >
+              <Text>임시 전송 버튼</Text>
+            </TouchableOpacity>
 
             {/* 태그 */}
             <Title>태그</Title>
@@ -552,7 +705,7 @@ const CreatePost: React.FC = () => {
             <ActionButton cancel={true}>
               <ActionButtonText cancel={true}>취소</ActionButtonText>
             </ActionButton>
-            <ActionButton cancel={false}>
+            <ActionButton cancel={false} onPress={submitPost}>
               <ActionButtonText cancel={false}>등록</ActionButtonText>
             </ActionButton>
           </BottomInfo>
@@ -705,15 +858,28 @@ const ContentInput = styled.TextInput`
 `;
 
 /* 이미지 업로드 */
-const PickImageButton = styled.TouchableOpacity`
-  width: 70px;
-  height: 40px;
+const ImageViewContainer = styled.View<{ imageCount: number }>`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: ${(props) =>
+    props.imageCount === 5 || props.imageCount === 10
+      ? "space-between"
+      : "flex-start"};
+  gap: 3px;
 `;
 
-const ProfileImage = styled.Image`
-  width: 100%;
-  height: 37px;
-  border: 1px solid #999;
+const PickImageButton = styled.TouchableOpacity`
+  width: 60px;
+  background-color: ${colors.primary};
+  border-radius: 5px;
+  text-align: center;
+  padding: 5px 7px;
+`;
+
+const PickImageButtonText = styled.Text`
+  color: ${colors.white};
+  text-align: center;
 `;
 
 /** 전체 공개, 비공개 설정 묶는 View */
