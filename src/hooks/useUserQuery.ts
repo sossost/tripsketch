99d6703eee7ba3f161b842386ba85e189892f02d
jang_yoonsu <shagrat8546@gmail.com>
2023-09-12 19:23:05 +0900

    Refactor : 유저 쿼리 훅 폴백 데이터 삭제

--- src/hooks/useUserQuery.ts
+++ src/hooks/useUserQuery.ts
@@ -8,40 +8,52 @@ import {
 import { QUERY_KEY } from "../react-query/queryKey";
 import { User } from "../types/user";
 
+/**
+ * @description : 현재 로그인한 유저의 정보를 요청하는 리액트 쿼리 훅
+ * @author : 장윤수
+ * @update : 2023-09-12,
+ * @version 1.0.0,
+ * @see None,
+ */
 export const useGetCurrentUser = () => {
-  const fallback = null;
-
-  const {
-    data = fallback as User | null,
-    isLoading,
-    isError,
-  } = useQuery<User | null>([QUERY_KEY.CURRENT_USER], getCurrentUser);
-
-  return { data, isLoading, isError };
-};
-
-export const useGetUserByNickname = (nickname: string) => {
-  const fallback = {
-    id: "",
-    email: "",
-    nickname: "",
-    introduction: "",
-    profileImageUrl: "",
-    followersCount: 0,
-    followingCount: 0,
-  };
-
-  const {
-    data = fallback as User,
-    isLoading,
-    isError,
-  } = useQuery<User>([QUERY_KEY.USER, nickname], () =>
-    getUserByNickname(nickname)
+  const { data, isLoading, isError } = useQuery<User | undefined>(
+    [QUERY_KEY.CURRENT_USER],
+    getCurrentUser
   );
 
   return { data, isLoading, isError };
 };
 
+/**
+ * @description : 닉네임으로 해당 유저의 정보를 요청하는 리액트 쿼리 훅
+ *
+ * @param nickname : 유저닉네임
+ *
+ * @author : 장윤수
+ * @update : 2023-09-12,
+ * @version 1.0.0,
+ * @see None,
+ */
+export const useGetUserByNickname = (nickname: string) => {
+  const { data, isLoading, isError } = useQuery<User>(
+    [QUERY_KEY.USER, nickname],
+    () => getUserByNickname(nickname)
+  );
+
+  return { data, isLoading, isError };
+};
+
+/**
+ * @description : 해당 페이지 유저의 팔로워 또는 팔로잉 리스트를 요청하는 리액트 쿼리 훅
+ *
+ * @param variant : 팔로워인지 팔로잉인지 variant
+ * @param pageUserNickname : 페이지 유저의 닉네임
+ *
+ * @author : 장윤수
+ * @update : 2023-09-12,
+ * @version 1.0.0,
+ * @see None,
+ */
 export const useGetSocialList = (
   variant: "팔로워" | "팔로잉",
   pageUserNickname: string
@@ -60,13 +72,10 @@ export const useGetSocialList = (
     }
   };
 
-  const fallback: User[] = [];
-
-  const {
-    data = fallback,
-    isLoading,
-    isError,
-  } = useQuery<User[] | undefined>(queryKey, queryFn);
+  const { data, isLoading, isError } = useQuery<User[] | undefined>(
+    queryKey,
+    queryFn
+  );
 
   return { data, isLoading, isError };
 };
