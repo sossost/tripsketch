d7cb2fa872dc3384b7556a28b4065909d2297173
jang_yoonsu <shagrat8546@gmail.com>
2023-09-12 16:30:24 +0900

    Refactor : 카카오 로그인페이지 필요없는 로직 삭제 및 리팩토링

--- src/components/auth/KakaoLoginPage.tsx
+++ src/components/auth/KakaoLoginPage.tsx
@@ -1,94 +1,88 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { View, StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
 import { useNavigation } from "@react-navigation/native";
-import * as SecureStore from "expo-secure-store";
-import { getCurrentUser, getUserInfo } from "../../services/user";
-import jwtDecode, { JwtPayload } from "jwt-decode";
+import { getCurrentUser } from "../../services/user";
 import axios from "axios";
 import { useQueryClient } from "@tanstack/react-query";
 import { QUERY_KEY } from "../../react-query/queryKey";
 import { KAKAO_CLIENT_ID } from "@env";
-import {
-  getDataFromSecureStore,
-  setDataToSecureStore,
-} from "../../utils/secureStore";
+import { setDataToSecureStore } from "../../utils/secureStore";
 import { STORE_KEY } from "../../constants/store";
 
-const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
-const clientId = KAKAO_CLIENT_ID!;
-const authorizeUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=https://port-0-tripsketch-kvmh2mljz6ccl7.sel4.cloudtype.app/api/oauth/kakao/callback&response_type=code`;
+import ErrorBoundary from "react-native-error-boundary";
+import ErrorFallback from "../UI/ErrorFallback";
+import { throwErrorMessage } from "../../utils/getErrorMessage";
 
-/** 카카오 로그인 페이지 컴포넌트 */
+const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
+const OAUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=https://port-0-tripsketch-kvmh2mljz6ccl7.sel4.cloudtype.app/api/oauth/kakao/callback&response_type=code`;
+
+/**
+ * @description : 카카오 OAuth 로그인 페이지 컴포넌트
+ * @author : 이수현
+ * @update : 2023-09-12, 장윤수, 필요없는 로직 삭제 리팩토링
+ * @version 1.0.1,
+ * @see None,
+ */
+
 const KaKaoLogin = () => {
   const navigation = useNavigation();
   const queryClient = useQueryClient();
 
-  // 카카오 로그인 진행하는 화면
-  const KakaoLoginWebView = (url: string) => {
-    requestToken(url);
-  };
-
   /** 토큰 발급받는 함수 */
   const requestToken = async (url: string) => {
     try {
-      const response = await axios.get(
-        `https://kauth.kakao.com/oauth/authorize?client_id=1927d084a86a31e01a814ce0b2fe3459&redirect_uri=https://port-0-tripsketch-kvmh2mljz6ccl7.sel4.cloudtype.app/api/oauth/kakao/callback&response_type=code`
-      );
+      const response = await axios.get(url);
 
       // 액세스 토큰 저장
-      const accessToken = response.headers.accesstoken;
-      await setDataToSecureStore(STORE_KEY.ACCESS_TOKEN, accessToken);
+      const accessToken: string = response.headers.accesstoken;
+      await setDataToSecureStore<string>(STORE_KEY.ACCESS_TOKEN, accessToken);
 
       // 리프레시 토큰 저장
-      const refreshToken = response.headers.refreshtoken;
-      await setDataToSecureStore(STORE_KEY.REFRESH_TOKEN, refreshToken);
+      const refreshToken: string = response.headers.refreshtoken;
+      await setDataToSecureStore<string>(STORE_KEY.REFRESH_TOKEN, refreshToken);
 
       // 리프레시 토큰 만료 기간 저장
       const refreshTokenExpiryDate = response.headers.refreshtokenexpirydate;
-      await SecureStore.setItemAsync(
+      await setDataToSecureStore(
         STORE_KEY.REFRESH_TOKEN_EXPIRED_DATE,
         refreshTokenExpiryDate
       );
-
-      const decodedToken: JwtPayload = jwtDecode(accessToken);
-
-      // accessToken의 만료 시간
-      const expiryDate = new Date(Number(decodedToken.exp) * 1000);
-      console.log(expiryDate);
-
-      // SecureStore에 저장된 accessToken값 가져오기
-      const tokenValue = await getDataFromSecureStore(STORE_KEY.ACCESS_TOKEN);
-
-      // 유저 정보를 SecureStore에 저장
-      const userInfo = await getCurrentUser();
-      queryClient.setQueryData([QUERY_KEY.CURRENT_USER], userInfo);
-      const userInfoFromSecureStore = await getUserInfo();
-
-      // 토큰 정상 발급되고, 유저 정보 저장 성공 후 메인 페이지로 이동
-      if (tokenValue && userInfoFromSecureStore) {
-        (navigation.navigate as (route: string) => void)("Home");
-      }
-    } catch (error) {
-      console.log(error);
+    } catch (error: unknown) {
+      throwErrorMessage(error, "토큰 발급 요청 실패 에러는");
     }
+  };
+
+  // 카카오 로그인 진행하는 화면
+  const KakaoLoginWebView = async (url: string) => {
+    // OAuth url에서 토근 발급 받은 후 스토어에 저장
+    requestToken(OAUTH_URL);
+
+    // 현재 로그인한 유저 정보 요청 후 쿼리에 저장
+    const userInfo = await getCurrentUser();
+    queryClient.setQueryData([QUERY_KEY.CURRENT_USER], userInfo);
+
+    // 홈 화면으로 이동
+    (navigation.navigate as (route: string) => void)("Home");
   };
 
   return (
     <View style={Styles.container}>
-      <WebView
-        style={{ flex: 1 }}
-        originWhitelist={["*"]}
-        scalesPageToFit={false}
-        source={{
-          uri: authorizeUrl,
-        }}
-        injectedJavaScript={INJECTED_JAVASCRIPT}
-        javaScriptEnabled
-        onMessage={(event) => {
-          KakaoLoginWebView(event.nativeEvent["url"]);
-        }}
-      />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <WebView
+          style={{ flex: 1 }}
+          originWhitelist={["*"]}
+          scalesPageToFit={false}
+          source={{
+            uri: OAUTH_URL,
+          }}
+          injectedJavaScript={INJECTED_JAVASCRIPT}
+          javaScriptEnabled
+          onMessage={(event) => {
+            KakaoLoginWebView(event.nativeEvent["url"]);
+          }}
+        />
+      </ErrorBoundary>
     </View>
   );
 };
