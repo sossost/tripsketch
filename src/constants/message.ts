4502732b135d4a20c1c40d2f202e4b83c2e2b0cf
jang_yoonsu <shagrat8546@gmail.com>
2023-09-12 19:30:19 +0900

    Feat : 메세지 상수 파일 추가

---
+++ src/constants/message.ts
@@ -0,0 +1,10 @@
+export const ERROR_MESSAGE = {
+  UNAUTHORIZED: "로그인이 필요합니다.",
+  FOLLOW: "팔로우를 실패하였습니다.",
+  UNFOLLOW: "팔로우 취소를 실패 하였습니다.",
+};
+
+export const SUCCESS_MESSAGE = {
+  FOLLOW: "팔로우 하였습니다.",
+  UNFOLLOW: "팔로우를 취소 하였습니다.",
+};
