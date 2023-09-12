c73e081b1abb2b583ba01fcbe4e3c35b9ec53e27
jang_yoonsu <shagrat8546@gmail.com>
2023-09-12 19:01:10 +0900

    Fix : 에러 메세지 유틸함수 throw 로직 제거

--- src/utils/getErrorMessage.ts
+++ src/utils/getErrorMessage.ts
@@ -21,8 +21,10 @@ export function getErrorMessage(error: unknown) {
 
 /**
  * @description : 에러를 받아서 콘솔로그에 띄우는 함수
+ *
  * @param error : 에러 unknown 타입
  * @param message : 콘솔로그에 띄울 메세지 앞부분
+ *
  * @author : 장윤수
  * @update : 2023-09-12,
  * @version 1.0.1, catch 블록안에서 throw 못해서 throw 로직 삭제
