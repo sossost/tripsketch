aafe7b8665a4d7de320f59e7e349a4bfba3eeacd
jang_yoonsu <shagrat8546@gmail.com>
2023-09-20 02:39:40 +0900

    Refactor : 유저 페이지 리팩토링

--- src/components/user/UserPageComponent.tsx
+++ src/components/user/UserPageComponent.tsx
@@ -26,24 +26,29 @@ interface UserPageComponentProps {
  * @param variant : 페이지 종류 (마이페이지, 유저페이지)
  *
  * @author : 장윤수
- * @update : 2023-09-19,
- * @version 1.4.1, 플랫리스트 스타일 변경
+ * @update : 2023-09-20,
+ * @version 1.4.2, 플랫리스트 스타일 변경
  * @see None,
  */
 const UserPageComponent = ({ nickname }: UserPageComponentProps) => {
+  // 현재 유저페이지의 선택된 카테고리 상태
+  const [selectedCategory, setSelectedCategory] = useRecoilState(categoryState);
+
   // 현재 로그인한 유저 정보를 가져옴
   const { data: currentUser } = useGetCurrentUser();
-  const isMypage = !nickname || nickname === currentUser!.nickname;
-  // 페이지 별로 헤더 왼쪽에 들어갈 컴포넌트를 다르게 렌더링
-  const LeftHeaderChildren = isMypage ? (
-    <Title title={"마이페이지"} />
-  ) : (
-    <CommonHeaderLeft title={nickname!} />
-  );
 
-  const [selectedCategory, setSelectedCategory] = useRecoilState(categoryState);
   // 현재 페이지 주인의 닉네임 (마이페이지면 현재 로그인한 유저의 닉네임)
   const pageOwnerNickname = nickname || currentUser!.nickname;
+
+  // 현재 페이지가 마이페이지인지 확인
+  const isMypage = pageOwnerNickname === currentUser?.nickname;
+
+  // 페이지 별로 헤더 왼쪽에 들어갈 컴포넌트를 다르게 렌더링
+  const LeftHeaderChildren = isMypage ? (
+    <Title title="마이페이지" />
+  ) : (
+    <CommonHeaderLeft title={pageOwnerNickname} />
+  );
 
   // 유저 페이지 변경시 카테고리 초기화
   useEffect(() => {
