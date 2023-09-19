b8f241ce95e3c0609ea2bfda4299d4b6f5fd3454
ksy <ksoup3434@gmail.com>
2023-09-19 18:55:53 +0900

    Feat: 게시물 생성 캘린더 컴포넌트 분리

--- src/components/post/PostWriteContainer.tsx
+++ src/components/post/PostWriteContainer.tsx
@@ -9,7 +9,6 @@ import {
   Alert,
 } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import { Calendar, LocaleConfig } from "react-native-calendars";
 import styled from "styled-components/native";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
@@ -27,6 +26,7 @@ import usePostTrip from "./hooks/usePostTrip";
 import useUpdatePost from "./hooks/useUpdatePost";
 import Loading from "../UI/Loading";
 import { resetStateStorage } from "./utils/resetStateStorage";
+import PostCalender from "./components/write/PostCalendar";
 
 type Suggestion = {
   place_id: string;
@@ -51,42 +51,6 @@ type ImagePickerResult = {
   exif?: { [key: string]: any };
   base64?: string;
 };
-
-/** 여행 글쓰기 */
-LocaleConfig.locales["ko"] = {
-  monthNames: [
-    "1월",
-    "2월",
-    "3월",
-    "4월",
-    "5월",
-    "6월",
-    "7월",
-    "8월",
-    "9월",
-    "10월",
-    "11월",
-    "12월",
-  ],
-  monthNamesShort: [
-    "1월",
-    "2월",
-    "3월",
-    "4월",
-    "5월",
-    "6월",
-    "7월",
-    "8월",
-    "9월",
-    "10월",
-    "11월",
-    "12월",
-  ],
-  dayNames: ["일", "월", "화", "수", "목", "금", "토"],
-  dayNamesShort: ["일", "월", "화", "수", "목", "금", "토"],
-  today: "오늘",
-};
-LocaleConfig.defaultLocale = "ko";
 
 /** 날짜 정보 Type */
 interface RangeKeyDict {
@@ -297,11 +261,6 @@ const PostWriteContainer: React.FC<PostPageProps> = ({
   /** 비공개 설정 핸들러 */
   const privateToggleHandler = () => {
     setIsPublic(false);
-  };
-
-  /** 달력 모달 닫는 핸들러 */
-  const modalCloseHandler = () => {
-    setShowModal(false);
   };
 
   type MapPressEvent = {
@@ -520,54 +479,6 @@ const PostWriteContainer: React.FC<PostPageProps> = ({
     }
   };
 
-  // react-native-calendars 패키지를 사용하여 달력을 구현 --------
-  /** 날짜 클릭 핸들러 */
-  const dayPressHandler = (day: { dateString: string }) => {
-    if (selectedDates.length === 0) {
-      setSelectedDates([day.dateString]);
-      setMarkedDates({ [day.dateString]: { color: "lightblue" } });
-    } else if (selectedDates.length === 1) {
-      const startDate = selectedDates[0];
-      const endDate = day.dateString;
-      const sortedDates = [startDate, endDate].sort();
-
-      const newMarkedDates: RangeKeyDict = {
-        [sortedDates[0]]: { startingDay: true, color: "lightblue" },
-        [sortedDates[1]]: { endingDay: true, color: "lightblue" },
-      };
-
-      const rangeDates = getRangeDates(sortedDates[0], sortedDates[1]);
-      Object.keys(rangeDates).forEach((date) => {
-        if (date !== sortedDates[0] && date !== sortedDates[1]) {
-          newMarkedDates[date] = { color: "lightblue" };
-        }
-      });
-
-      setMarkedDates(newMarkedDates);
-      setSelectedDates(sortedDates);
-      setStartDate(sortedDates[0]);
-      setEndDate(sortedDates[1]);
-    } else if (selectedDates.length >= 2) {
-      setSelectedDates([day.dateString]);
-      setMarkedDates({ [day.dateString]: { color: "lightblue" } });
-      setStartDate(null);
-      setEndDate(null);
-    }
-  };
-
-  /** 사이 날짜 구하는 함수 */
-  const getRangeDates = (start: string, end: string) => {
-    const rangeDates: RangeKeyDict = {};
-    const currentDate = new Date(start);
-    while (currentDate <= new Date(end)) {
-      rangeDates[currentDate.toISOString().split("T")[0]] = {
-        color: "lightblue",
-      };
-      currentDate.setDate(currentDate.getDate() + 1);
-    }
-    return rangeDates;
-  };
-
   const cancelPost = () => {
     Alert.alert("알림", "작성을 취소하고 이전 페이지로 돌아가시겠습니까?", [
       {
@@ -599,6 +510,8 @@ const PostWriteContainer: React.FC<PostPageProps> = ({
     deletedImageUrls: deleteUpdateImage,
   };
 
+  //console.log(postTripData);
+
   const { submitPost, isLoading } = usePostTrip(postTripData);
   const { submitUpdatePost, isUpdateLoading } = useUpdatePost(postTripData);
 
@@ -614,110 +527,18 @@ const PostWriteContainer: React.FC<PostPageProps> = ({
         ) : null}
         <HeaderInfo>
           {/* 여행기간 */}
-          <InfoBox>
-            <CalendarIcon name="calendar" />
-            <Title>여행기간</Title>
-
-            <ContentText>
-              <Text onPress={() => setShowModal(true)}>
-                {startDate === null
-                  ? "날짜를 선택해주세요."
-                  : `${startDate} ~ ${endDate}`}
-              </Text>
-            </ContentText>
-
-            {/* 날짜 선택 클릭시 달력에서 여행 기간 선택 */}
-            {showModal && (
-              <Modal visible={showModal} animationType="fade">
-                <ModalContainer>
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      width: "80%",
-                      height: 65, // 레이블들이 고정된 높이를 가지도록 추가
-                      borderRadius: 10,
-                      backgroundColor: "white",
-                    }}
-                  >
-                    {/* <Text>selectedDates : {selectedDates}</Text> */}
-                    <View
-                      style={{
-                        alignItems: "center",
-                        justifyContent: "center",
-                        flex: 1,
-                      }}
-                    >
-                      <Text
-                        style={{
-                          textAlign: "center",
-                          color: "#6F6F6F",
-                          marginBottom: "3%",
-                        }}
-                      >
-                        시작일
-                      </Text>
-                      <Text style={{ fontSize: 16, color: "#414141" }}>
-                        {startDate}
-                      </Text>
-                    </View>
-
-                    <View
-                      style={{
-                        alignItems: "center",
-                        justifyContent: "center",
-                        flex: 1,
-                      }}
-                    >
-                      <Text
-                        style={{
-                          textAlign: "center",
-                          color: "#6F6F6F",
-                          marginBottom: "3%",
-                        }}
-                      >
-                        종료일
-                      </Text>
-                      <Text style={{ fontSize: 16, color: "#414141" }}>
-                        {endDate}
-                      </Text>
-                    </View>
-                  </View>
-
-                  {/* 달력 모달 닫기 버튼 */}
-                  <CloseButton onPress={modalCloseHandler}>
-                    <CloseIcon name="x" />
-                  </CloseButton>
-                  <Calendar
-                    style={{
-                      borderRadius: 20,
-                      marginTop: 20,
-                      width: "90%",
-                      aspectRatio: 0.9,
-                    }}
-                    onDayPress={dayPressHandler}
-                    markingType={"period"}
-                    markedDates={markedDates}
-                  />
-                  <TouchableOpacity
-                    style={{
-                      width: "30%",
-                      height: "6%",
-                      backgroundColor: "white",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      borderRadius: 10,
-                      marginTop: 10,
-                    }}
-                    onPress={modalCloseHandler}
-                  >
-                    <Text style={{ fontSize: 18 }}>선택하기</Text>
-                  </TouchableOpacity>
-                </ModalContainer>
-              </Modal>
-            )}
-          </InfoBox>
+          <PostCalender
+            showModal={showModal}
+            setShowModal={setShowModal}
+            markedDates={markedDates}
+            setMarkedDates={setMarkedDates}
+            selectedDates={selectedDates}
+            setSelectedDates={setSelectedDates}
+            startDate={startDate}
+            setStartDate={setStartDate}
+            endDate={endDate}
+            setEndDate={setEndDate}
+          />
 
           {/* 여행지 */}
           <InfoBox>
