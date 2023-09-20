4f6cc1c9f2acab51bc401ed135b2aea1a05e36b5
jang_yoonsu <shagrat8546@gmail.com>
2023-09-20 15:33:47 +0900

    Refactor : 탐색 페이지 리팩토링

--- src/components/explore/SearchPostList.tsx
+++ src/components/explore/SearchPostList.tsx
@@ -1,12 +1,10 @@
-import { FlatList, RefreshControl } from "react-native";
-import { Post } from "../../types/Post";
-import { useGetPostsBySearchQuery } from "../../hooks/usePostQuery";
+import { useGetPostsBySearchQuery } from "@hooks/usePostQuery";
 import { useCallback } from "react";
 import { useQueryClient } from "@tanstack/react-query";
-import { QUERY_KEY } from "../../react-query/queryKey";
+import { QUERY_KEY } from "@react-query/queryKey";
 
-import HorizontalPostCard from "../post/card/HorizontalPostCard";
-import Loading from "../UI/Loading";
+import Loading from "@components/UI/Loading";
+import HorizontalPostFlatList from "@components/post/HorizontalPostFlatList";
 
 interface SearchPostListProps {
   searchQuery: string;
@@ -20,7 +18,7 @@ interface SearchPostListProps {
  * @param variant : 최신순, 인기순 variant
  *
  * @author : 장윤수
- * @update : 2023-09-19,
+ * @update : 2023-09-20,
  * @version 1.1.0, 무한 스크롤 패치중 로딩 컴포넌트 추가
  * @see None,
  */
@@ -53,27 +51,12 @@ const SearchPostList = ({ searchQuery, variant }: SearchPostListProps) => {
   };
 
   return (
-    <>
-      <FlatList
-        data={posts}
-        renderItem={({ item }) => (
-          <HorizontalPostCard key={(item as Post).id} post={item as Post} />
-        )}
-        contentContainerStyle={{
-          gap: 10,
-          paddingVertical: 20,
-          paddingHorizontal: 2,
-        }}
-        alwaysBounceVertical={false}
-        onEndReached={handleEndReached}
-        onEndReachedThreshold={0.1}
-        showsVerticalScrollIndicator={false}
-        refreshControl={
-          <RefreshControl refreshing={false} onRefresh={handleRefresh} />
-        }
-        ListFooterComponent={renderFooter()}
-      />
-    </>
+    <HorizontalPostFlatList
+      posts={posts}
+      handleEndReached={handleEndReached}
+      handleRefresh={handleRefresh}
+      listFooterComponent={renderFooter()}
+    />
   );
 };
 
--- src/components/post/HorizontalPostFlatList.tsx
+++ src/components/post/HorizontalPostFlatList.tsx
@@ -1,12 +1,14 @@
 import { FlatList } from "react-native";
-import { Post } from "../../types/Post";
+import { Post } from "@types/Post";
 import { ReactElement } from "react";
+import { RefreshControl } from "react-native-gesture-handler";
 
-import HorizontalPostCard from "./card/HorizontalPostCard";
+import HorizontalPostCard from "@components/post/card/HorizontalPostCard";
 
 interface DiaryListProps {
   posts: Post[];
   handleEndReached: () => void;
+  handleRefresh: () => void;
   listHeaderComponent?: ReactElement;
   listFooterComponent?: ReactElement;
 }
@@ -16,21 +18,22 @@ interface DiaryListProps {
  *
  * @param posts : 포스트 리스트
  * @param handleEndReached : 스크롤 끝까지 내렸을때 다음페이지 패치하는 핸들러
+ * @param handleRefresh : 리스트 아래로 당기면 데이터 리프레쉬 하는 핸들러
  * @param listHeaderComponent : 리스트 헤더 컴포넌트
  * @param listFooterComponent : 리스트 푸터 컴포넌트
  *
  * @author : 장윤수
- * @update : 2023-09-17,
- * @version 1.0.0,
+ * @update : 2023-09-20,
+ * @version 1.1.0, 리프레시 핸들러 추가
  * @see None,
  */
 const HorizontalPostFlatList = ({
   posts,
   handleEndReached,
+  handleRefresh,
   listHeaderComponent,
   listFooterComponent,
 }: DiaryListProps) => {
-  console.log(posts);
   return (
     <FlatList
       data={posts}
@@ -48,6 +51,9 @@ const HorizontalPostFlatList = ({
       ListHeaderComponent={listHeaderComponent}
       ListFooterComponent={listFooterComponent}
       showsVerticalScrollIndicator={false}
+      refreshControl={
+        <RefreshControl refreshing={false} onRefresh={handleRefresh} />
+      }
     />
   );
 };
