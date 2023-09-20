82f4d0b74d4ec8e0dac412207ddf576c504ff516
jang_yoonsu <shagrat8546@gmail.com>
2023-09-20 13:10:04 +0900

    Chore : 절대경로 세팅

--- metro.config.js
+++ metro.config.js
@@ -8,6 +8,13 @@ module.exports = {
       "@components": "./src/components",
       "@screens": "./src/screens",
       "@assets": "./src/assets",
+      "@utils": "./src/utils",
+      "@constants": "./src/constants",
+      "@hooks": "./src/hooks",
+      "@react-query": "./src/react-query",
+      "@navigation": "./src/navigation",
+      "@services": "./src/services",
+      "@types": "./src/types",
     },
   },
   transformer: {
