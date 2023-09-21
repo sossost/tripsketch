e34282f9a018646c8d93a4ce1cb4b09fafffc15e
jang_yoonsu <shagrat8546@gmail.com>
2023-09-21 19:49:14 +0900

    Design : 오픈소스 페이지 스타일 조정

--- src/components/setup/policy/OpenSourcePageComponent.tsx
+++ src/components/setup/policy/OpenSourcePageComponent.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Text } from "react-native";
+import { Dimensions, FlatList, Text } from "react-native";
 import { OpenSourceLicense } from "./OpenSourceLicense";
 import { styled } from "styled-components/native";
 import { useNavigation } from "@react-navigation/native";
@@ -14,26 +14,36 @@ const OpenSourcePageComponent = () => {
   const navigation = useNavigation<StackNavigation>();
 
   return (
-    <PageLayout>
-      <Header left={<CommonHeaderLeft title="오픈소스 라이선스" />} />
+    <PageLayout style={{ paddingHorizontal: 0 }}>
+      <Header
+        left={<CommonHeaderLeft title="오픈소스 라이선스" />}
+        style={{ paddingHorizontal: 20 }}
+      />
       <FlatList
         data={OpenSourceLicense}
         renderItem={({ item }) => (
-          <ListWrapper
-            onPress={() =>
-              navigation.navigate(LINK.OPENSOURCE_LICENCE_DETAIL_PAGE, {
-                libraryName: item.libraryName,
-              })
-            }
-          >
-            <ListContent>
-              <ListTitle>{item.libraryName}</ListTitle>
-              <Text>Version. {item.version}</Text>
-            </ListContent>
-            <NavigationButton>
-              <NavigationIcon source={require("@assets/images/nextIcon.png")} />
-            </NavigationButton>
-          </ListWrapper>
+          <>
+            <ListWrapper
+              onPress={() =>
+                navigation.navigate(LINK.SETUP.OPENSOURCE_LICENCE_DETAIL_PAGE, {
+                  libraryName: item.libraryName,
+                })
+              }
+            >
+              <ListContent>
+                <ListTitle numberOfLines={1} ellipsizeMode="tail">
+                  {item.libraryName.substring(1)}
+                </ListTitle>
+                <Text>Version. {item.version}</Text>
+              </ListContent>
+              <NavigationButton>
+                <NavigationIcon
+                  source={require("@assets/images/nextIcon.png")}
+                />
+              </NavigationButton>
+            </ListWrapper>
+            <Line />
+          </>
         )}
         keyExtractor={(item) => String(item.libraryName)}
         showsVerticalScrollIndicator={false}
@@ -49,19 +59,22 @@ const ListWrapper = styled.TouchableOpacity`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding: 10px;
+  padding: 10px 20px;
+  width: 100%;
 `;
 
 const ListTitle = styled.Text`
   font-size: 20px;
   font-weight: 500;
   color: ${colors.mainFont};
+  width: 90%;
 `;
 
 const ListContent = styled.View`
   display: flex;
   flex-direction: column;
   gap: 5px;
+  flex: 1;
 `;
 
 const NavigationButton = styled.View`
@@ -73,4 +86,9 @@ const NavigationButton = styled.View`
 const NavigationIcon = styled.Image`
   width: 10px;
   height: 18px;
+`;
+
+const Line = styled.View`
+  height: 1px;
+  background-color: ${colors.lightGrey};
 `;
