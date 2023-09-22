df6d31e122bfbdfaaaaf5fa30499adb9f599ecdf
jang_yoonsu <shagrat8546@gmail.com>
2023-09-22 19:39:08 +0900

    Fix : env 타입 수정

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,4 @@
 declare module "@env" {
-  export const S3_ACCESS_KEY_ID: string;
-  export const S3_SECRET_ACCESS_KEY: string;
-  export const S3_BUCKET_NAME: string;
   export const API_BASE_URL: string;
-  export const KAKAO_CLIENT_ID: string;
+  export const NODE_ENV: "production" | "development";
 }
