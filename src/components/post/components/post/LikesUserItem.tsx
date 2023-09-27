6a8c240532b308165ce475758b4403dbb4fd767c
ksy <ksoup3434@gmail.com>
2023-09-28 05:10:40 +0900

    Fix: 유저페이지 비회원 접근 분기처리

--- src/components/post/components/post/LikesUserItem.tsx
+++ src/components/post/components/post/LikesUserItem.tsx
@@ -1,9 +1,10 @@
 import { colors } from "@constants/color";
-import { Platform } from "react-native";
+import { Alert, Platform } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import styled from "styled-components/native";
 import { StackNavigation } from "@types/RootStack";
 import { LINK } from "@constants/link";
+import { useGetCurrentUser } from "@hooks/useUserQuery";
 
 type LikesItemProps = {
   likesData: any;
@@ -11,10 +12,25 @@ type LikesItemProps = {
 };
 
 const LikesUserItem = ({ likesData, modalClose }: LikesItemProps) => {
+  const { data: userData } = useGetCurrentUser();
   const navigation = useNavigation<StackNavigation>();
+
   const userPageHandler = () => {
     navigation.navigate(LINK.USER_PAGE, { nickname: likesData.nickname });
     modalClose();
+  };
+
+  // 비회원일 경우 로그인 페이지로 이동
+  const loginHandler = () => {
+    Alert.alert("알림", "로그인이 필요한 서비스입니다.", [
+      {
+        text: "확인",
+        onPress: () => {
+          modalClose();
+          navigation.navigate(LINK.MY_PAGE);
+        },
+      },
+    ]);
   };
 
   return (
@@ -26,7 +42,7 @@ const LikesUserItem = ({ likesData, modalClose }: LikesItemProps) => {
         <NickNamebox>{likesData.nickname}</NickNamebox>
       </InfoBox>
       <ButtonBox>
-        <UserPageButton onPress={userPageHandler}>
+        <UserPageButton onPress={userData ? userPageHandler : loginHandler}>
           <ButtonText>유저페이지</ButtonText>
         </UserPageButton>
       </ButtonBox>
